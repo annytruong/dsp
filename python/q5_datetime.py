f6b5001f01fef3bd7294217228ebb1c776e5b054
annytruong <truonga87@yahoo.com>
2016-03-26 11:31:46 -0700

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,27 @@
 # Hint:  use Google to find python function
 
+from datetime import datetime
+
+def days_between(date_start, date_stop):
+	return abs((date_stop - date_start).days)
+
 ####a) 
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
+date_start = datetime.strptime(date_start, "%m-%d-%Y")
+date_stop = datetime.strptime(date_stop, "%m-%d-%Y")
+print days_between(date_start, date_stop)
 
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
+date_start = datetime.strptime(date_start, "%m%d%Y")
+date_stop = datetime.strptime(date_stop, "%m%d%Y")
+print days_between(date_start, date_stop)
 
 ####c)  
 date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_stop = '14-Jul-2015'
+date_start = datetime.strptime(date_start, "%d-%b-%Y")
+date_stop = datetime.strptime(date_stop, "%d-%b-%Y")
+print days_between(date_start, date_stop)
